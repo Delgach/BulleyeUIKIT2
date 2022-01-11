45e1f5073f0f90d05bfb9e89a2d119617256de20
Vladimir D <vladimir_rds@icloud.com>
2022-01-11 20:52:45 +0300

    Initial Commit

---
+++ BulleyeUIKIT2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BulleyeUIKIT2
+//
+//  Created by Владимир Дельгадильо on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
