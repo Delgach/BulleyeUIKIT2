5c8006b13c7777affcc2c800b3f597c7fa117df9
Vladimir D <vladimir_rds@icloud.com>
2022-01-11 21:20:32 +0300

    first commit

--- BulleyeUIKIT2/ViewController.swift
+++ BulleyeUIKIT2/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var goal: UILabel!
+    @IBOutlet var score: UILabel!
+    @IBOutlet var round: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func handleSubmit() -> Void {
+        let alert = UIAlertController(title: "some title", message: "some message", preferredStyle: .alert)
+        let action = UIAlertAction(title: "some action", style: .cancel) {_ in
+            self.goal.text = "\(Int.random(in: 1...100))"
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func handleStartNewGame() -> Void {
+        
     }
 
 
